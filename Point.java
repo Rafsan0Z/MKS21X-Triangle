0376819476c2b35d6c8cbf6b4a863ebedde618d0
RoboticsRafsan01 <mzaman1@stuy.edu>
2018-10-10 18:43:04 -0400

    Point File done!

--- Point.java
+++ Point.java
@@ -17,5 +17,27 @@ public class Point{
 
   public double getY(){
     return y;
-  }  
+  }
+
+	public double distance(Point P1, Point P2) {
+		double p1x = P1.x;
+		double p1y = P1.y;
+		double p2x = P2.x;
+		double p2y = P2.y; 
+
+		double Part1 = Math.abs((p1x - p2x)*(p1x - p2x));
+		double Part2 = Math.abs((p1y - p2y)*(p1y - p2y));
+
+		double total = Math.sqrt(Part1 + Part2);
+		return total;
+	}
+
+	public double distanceTo(Point P2) {
+
+		double p1x = this.x;
+		double p1y = this.y;
+			
+		Point P1 = new Point(p1x,p1y);
+		return distance(P1,P2);
+	}
 }
